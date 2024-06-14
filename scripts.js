c190c4fd16d1813eb8ac365a6f4dc6066eb95424
neyadllbot <karahennlich@gmail.com>
2024-06-14 16:24:19 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function showClothes() {
+    alert('Показать одежду');
+}
+
+function showHairstyles() {
+    alert('Показать прически');
+}
+
+function showInventory() {
+    alert('Показать инвентарь');
+}
+
+function showStats() {
+    alert('Показать статы');
+}
